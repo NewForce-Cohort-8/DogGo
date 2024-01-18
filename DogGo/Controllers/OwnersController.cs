45faffde421ce621a303d736bc30d741c2e142d4
Sarah Brooks <sarahebrooks12@gmail.com>
2024-01-18 11:00:39 -0500

    review done to challenge

---
+++ DogGo/Controllers/OwnersController.cs
@@ -0,0 +1,101 @@
+﻿using DogGo.Models;
+using DogGo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogGo.Controllers
+{
+    public class OwnersController : Controller
+    {
+        private readonly IOwnerRepository _ownerRepo;
+
+        // ASP.NET will give us an instance of our Walker Repository. This is called "Dependency Injection"
+        public OwnersController(IOwnerRepository ownerRepository)
+        {
+            _ownerRepo = ownerRepository;
+        }
+        // GET: Owners
+        public ActionResult Index()
+        {
+            List<Owner> owners = _ownerRepo.GetAllOwners();
+
+            return View(owners);
+        }
+
+        // GET: Owners/Details/5
+        public ActionResult Details(int id)
+        {
+            Owner owner = _ownerRepo.GetOwnerById(id);
+
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            return View(owner);
+        }
+
+        // GET: Owners/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Owners/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Owners/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Owners/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Owners/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Owners/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DogGo/Repositories/OwnerRepository.cs
@@ -0,0 +1,120 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly IConfiguration _config;
+
+        // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
+        public OwnerRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Owner> GetAllOwners()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // name, address, phone, email, neighborhoodId
+                    cmd.CommandText = @"
+                        SELECT Id, [Name], Address, NeighborhoodId, Phone, Email
+                        FROM Owner
+                    ";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Owner> owners = new List<Owner>();
+                    while (reader.Read())
+                    {
+                        Owner owner = new Owner
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Address = reader.GetString(reader.GetOrdinal("Address")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                            Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                            Email = reader.GetString(reader.GetOrdinal("Email"))
+
+                        };
+
+                        owners.Add(owner);
+                    }
+
+                    reader.Close();
+
+                    return owners;
+                }
+            }
+        }
+
+        public Owner GetOwnerById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        Select Owner.Id, Owner.[Name], Owner.Address, Owner.NeighborhoodId, Owner.Phone, Owner.Email, Dog.Id as 'Dog Id', Dog.[Name] as 'Dog Name', Dog.Breed
+        from Owner
+        left join Dog on Dog.OwnerId = Owner.Id
+        where Owner.Id = @id;
+                    ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    Owner owner = null;
+                    while (reader.Read())
+                    {
+                        if (owner == null)
+                        {
+
+                            owner = new Owner
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Address = reader.GetString(reader.GetOrdinal("Address")),
+                                NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                                Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                                Email = reader.GetString(reader.GetOrdinal("Email")),
+                                Dogs = new List<Dog>()
+                            };
+
+                        }
+                        if (!reader.IsDBNull(reader.GetOrdinal("Dog Id")))
+                        {
+                            Dog dog = new Dog
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Dog Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Dog Name")),
+                                Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                                Notes = null,
+                                ImageUrl = null,
+                                OwnerId = reader.GetInt32(reader.GetOrdinal("Id"))
+                            };
+                            owner.Dogs.Add(dog);
+                        }
+                    }
+                        reader.Close();
+                        return owner;
+                    }
+                }
+            }
+        }
+    }
